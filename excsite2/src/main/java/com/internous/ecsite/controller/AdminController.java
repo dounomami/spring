32c0c841eb8e56f0452c44e354d2fbbb870dd842
dounomami <mami.un2@icloud.com>
2021-01-07 20:28:50 +0900

    最初のコミット

---
+++ excsite2/src/main/java/com/internous/ecsite/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.internous.ecsite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ecsite/admin")
+public class AdminController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "adminindex";
+	}
+
+}
